408a5ec8d0c8a53751c2ab5881c947824ce90e8c
GregRitacco <92963387+GregRitacco@users.noreply.github.com>
2022-01-10 21:39:19 -0800

    Initial

---
+++ Install/QKInstall.bat
@@ -0,0 +1,5 @@
+
+MD "%homepath%\AppData\Roaming\TrainPlayer\SubRoutines\QuickKeys-Base"
+XCopy QK\*.* "%homepath%\AppData\Roaming\TrainPlayer\SubRoutines\QuickKeys-Base" /i /y /s
+
+xCopy Defaults\*.*  "%homepath%\AppData\Roaming\TrainPlayer\Cartypes\Defaults" /y /s
