fec14d7676ab22b8ffae08e5fdce607d1b441188
GregRitacco <greg95747@gmail.com>
2023-05-21 10:07:40 -0700

    Working

--- SetUpQuickKeys.bat
+++ SetUpQuickKeys.bat
@@ -3,6 +3,18 @@
 set "qktDir=%homepath%\AppData\Roaming\TrainPlayer\Subroutines\QuickKeys\"
 xcopy %cd% %qktDir% /H /S /C /Y
 
+set "sourceFile=%homepath%\AppData\Roaming\TrainPlayer\Subroutines\QuickKeys\qkSupport\PressX.txt"
+xcopy %sourceFile% %qktDir% /C /Y
+
+set "sourceFile=%homepath%\AppData\Roaming\TrainPlayer\Subroutines\QuickKeys\qkSupport\PressY.txt"
+xcopy %sourceFile% %qktDir% /C /Y
+
+set "sourceFile=%homepath%\AppData\Roaming\TrainPlayer\Subroutines\QuickKeys\qkSupport\PressZ.txt"
+xcopy %sourceFile% %qktDir% /C /Y
+
+set "sourceFile=%homepath%\AppData\Roaming\TrainPlayer\Subroutines\QuickKeys\qkSupport\PersonalizedSettings.txt"
+xcopy %sourceFile% %qktDir% /C /Y
+
 set "dksDir=%cd%\dkSupport\"
 set "dktDir=%homepath%\AppData\Roaming\TrainPlayer\Cartypes\Defaults\"
 xcopy %dksDir% %dktDir% /H /S /C /Y
